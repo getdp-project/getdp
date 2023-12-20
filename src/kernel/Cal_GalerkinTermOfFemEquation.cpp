3a681eb6629fa2734821c78c42db9f9ebf07dbac
Christophe Geuzaine <cgeuzaine@uliege.be>
2023-12-20 10:08:27 +0100

    workaroud for nonzero cmake/ctest exit values even on success

--- src/kernel/Cal_GalerkinTermOfFemEquation.cpp
+++ src/kernel/Cal_GalerkinTermOfFemEquation.cpp
@@ -721,7 +721,7 @@ void Cal_GalerkinTermOfFemEquation(struct Element *Element,
       } /* if NextElement */
     } /* if INTEGRALQUANTITY */
 
-#if 0
+#if 1
     if(Message::GetCommSize() > 1) {
       // if all the equations lead to matrix entries and they are all outside
       // the range owned by the current process, skip the assembly altogether:
