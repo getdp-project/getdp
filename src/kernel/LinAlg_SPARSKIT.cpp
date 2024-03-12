90b9ab529b434dce0587962d048ac23bdded29f0
Christophe Geuzaine <cgeuzaine@uliege.be>
2024-03-12 08:14:56 +0100

    fix compile with sparskit (#167)

--- src/kernel/LinAlg_SPARSKIT.cpp
+++ src/kernel/LinAlg_SPARSKIT.cpp
@@ -117,7 +117,7 @@ void LinAlg_CreateVector(gVector *V, gSolver *Solver, int n)
   V->N = n;
 }
 
-void LinAlg_CreateMatrix(gMatrix *M, gSolver *Solver, int n, int m)
+void LinAlg_CreateMatrix(gMatrix *M, gSolver *Solver, int n, int m, bool silent)
 {
   init_matrix(n, &M->M, &Solver->Params);
 }
