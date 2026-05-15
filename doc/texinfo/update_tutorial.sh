333890dedde1591c1d310fc740fd5645f4a13dba
Christophe Geuzaine <cgeuzaine@uliege.be>
2026-05-15 22:04:48 +0200

    update tuto links

--- doc/texinfo/update_tutorial.sh
+++ doc/texinfo/update_tutorial.sh
@@ -21,7 +21,7 @@ tuto() {
     sed -i '' -e 's/GetDP tutorial/Tutorial/g' tmp.texi
     sed -i '' -e "/^@menu/i\\
 @center@image{images/$1,14cm,,Screenshot of tutorial $1}\\
-@center See @url{https://gitlab.onelab.info/getdp/tutorials/-/tree/main/$1,tutorials/$1}.\\
+@center See @url{https://gitlab.onelab.info/getdp/getdp/-/tree/master/tutorials/$1,tutorials/$1}.\\
 " tmp.texi
     sed -i '' -e '/^@menu$/d' tmp.texi
     sed -i '' -e '/^* Features::$/d' tmp.texi
